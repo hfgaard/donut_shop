3d699799d237547a7718f9465ccc976fd218de99
Hana Fallisgaard <hfgaard@gmail.com>
2015-05-19 13:07:13 -0700

    Refactor code.
    Removed functionality to print to the DOM from bakePerDay function.
    Added functionality to print to the DOM from the render function.

--- donuts.js
+++ donuts.js
@@ -4,13 +4,10 @@ function DonutShop(name, minHourlyCustomers, maxHourlyCustomers, aveDonutsPerCus
   this.minHourlyCustomers  = minHourlyCustomers;
   this.maxHourlyCustomers = maxHourlyCustomers;
   this.aveDonutsPerCustomer = aveDonutsPerCustomer;
+  this.donutsPerHour = [];
+  this.totalDonuts = this.bakePerDay();
 }
   
-DonutShop.prototype.render = function(perHour) {
-  var node = document.createElement("td");
-  node.innerHTML = perHour;
-  return node;
-}
 
 DonutShop.prototype.purchasingCustomers = function() {
   return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
@@ -20,28 +17,46 @@ DonutShop.prototype.bakePerHour = function() {
   return Math.floor(this.purchasingCustomers() * this.aveDonutsPerCustomer);
 }
 
-DonutShop.prototype.bakePerDay = function(id) {
+DonutShop.prototype.bakePerDay = function() {
   var total = 0;
-  var baked = 0;
-  var parent = document.getElementById(id);
-  parent.appendChild(this.render(this.name));
-
   for (var i = 0; i <= this.operationsHours; i++) {
-    baked = this.bakePerHour();
-    total += baked;
-    parent.appendChild(this.render(baked));
+    var donutsPerHour = this.bakePerHour();
+    this.donutsPerHour[i] = donutsPerHour;
+    total += donutsPerHour;
   }
-  parent.appendChild(this.render(total));
+  return total
 }
 
-var downtown = new DonutShop("Downtown", 8, 43, 4.50);
-var caphill = new DonutShop("Capitol Hill", 4, 37, 2.00);
-var slu = new DonutShop("South Lake Union", 9, 23, 6.33);
-var wedgewood = new DonutShop("Wedgewood", 2, 28, 1.25);
-var ballard = new DonutShop("Ballard", 8, 58, 3.75);
+DonutShop.prototype.renderData = function() {
+  var node, row, tableLocation;
+  
+  node = document.createElement("td");
+  node.textContent = this.name;
+  
+  row = document.createElement("tr");
+  row.appendChild(node);
 
-downtown.bakePerDay("downtown");
-caphill.bakePerDay("caphill");
-slu.bakePerDay("slu");
-wedgewood.bakePerDay("wedgewood");
-ballard.bakePerDay("ballard");
+  tableLocation = document.getElementById("donut-table");
+  tableLocation.appendChild(row);
+
+  for (var i = 0; i <= this.operationsHours; i++) {
+    node = document.createElement("td");
+    node.textContent = this.donutsPerHour[i];
+    row.appendChild(node);
+  }
+  
+  node = document.createElement("td");
+  node.textContent = this.totalDonuts;
+  row.appendChild(node);
+}
+
+var locations = [];
+locations.push(new DonutShop("Downtown", 8, 43, 4.50));
+locations.push(new DonutShop("Capitol Hill", 4, 37, 2.00));
+locations.push(new DonutShop("South Lake Union", 9, 23, 6.33));
+locations.push(new DonutShop("Wedgewood", 2, 28, 1.25));
+locations.push(new DonutShop("Ballard", 8, 58, 3.75));
+
+for (var i = 0; i < locations.length; i++) {
+  locations[i].renderData();
+}
